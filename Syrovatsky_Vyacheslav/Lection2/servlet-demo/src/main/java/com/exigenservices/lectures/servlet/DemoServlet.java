97cd3a6186dd0053272b33b5b609c1f089006cb0
Vyacheslav Syrovatsky <slasyz@gmail.com>
2015-12-12 04:24:50 +0300

    Fixed DemoServlet.

--- Syrovatsky_Vyacheslav/Lection2/servlet-demo/src/main/java/com/exigenservices/lectures/servlet/DemoServlet.java
+++ Syrovatsky_Vyacheslav/Lection2/servlet-demo/src/main/java/com/exigenservices/lectures/servlet/DemoServlet.java
@@ -2,6 +2,8 @@ package com.exigenservices.lectures.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,12 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
         res.setContentType("text/html");
 
         PrintWriter writer = res.getWriter();
         writer.println("<html>");
         writer.println("<head><title>My first page</title></head>");
-        writer.println("<body>This is my first page</body>");
+        writer.println("<body>Current time is " + sdf.format(cal.getTime()) + "</body>");
         writer.println("</html>");
         writer.close();
     }
